a5e98e670ae2f2f8fb13492217e836550d470273
zhaohaojie1998 <1322684717@qq.com>
2024-03-19 20:55:14 +0800

    动态路径规划demo程序

--- demo_dynamic.py
+++ demo_dynamic.py
@@ -6,75 +6,28 @@
  
  @auther: HJ https://github.com/zhaohaojie1998
 """
-
-from copy import deepcopy
+#
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 plt.close('all')
 
-import torch as th
-import numpy as np
-import torch.nn as nn
-from copy import deepcopy
-
-
-
-'''策略定义'''
-class EncoderNet(nn.Module):
-    def __init__(self, obs_space, feature_dim):
-        super(EncoderNet, self).__init__()
-        c, point_dim = obs_space['point'].shape
-        state_dim = obs_space['state'].shape[0]
-        self.cnn = nn.Sequential(
-
-        )
-        self.mlp = nn.Sequential(
-            nn.Linear(state_dim, feature_dim),
-            nn.ReLU(True)
-        )
-        self.fusion = nn.Sequential(
-            nn.Linear(2*feature_dim, feature_dim),
-            nn.ReLU(True)
-        )
-    def forward(self, obs):
-        f1 = self.cnn(obs['point'])
-        f2 = self.mlp(obs['state'])
-        return self.fusion(th.cat([f1, f2], -1))
-    
-class PNet(nn.Module):
-    def __init__(self, feature_dim, act_dim):
-        super(PNet, self).__init__()
-        self.mlp = nn.Sequential(
-            nn.Linear(feature_dim, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 64),
-            nn.ReLU(True),
-            nn.Linear(64, act_dim)
-        )
-    def forward(self, feature):
-        return self.mlp(feature)
-
 
 '''环境实例化'''
-from env import DynamicPathPlanning, NormalizedActionsWrapper
-env = NormalizedActionsWrapper(DynamicPathPlanning())
+from env import DynamicPathPlanning
+env = DynamicPathPlanning() # 动作空间本身就是 -1,1
 obs_space = env.observation_space
 act_dim = env.action_space.shape[0]
 
 
-'''策略实例化'''
-from sac_agent import SAC_Actor
-policy = SAC_Actor(
-        EncoderNet(obs_space, 256),
-        PNet(256, act_dim),
-        PNet(256, act_dim),
-    )
-policy.load_state_dict(th.load("demo_dynamic.pth", map_location="cpu"))
+'''策略加载'''
+import onnxruntime as ort
+policy = ort.InferenceSession("policy_dynamic.onnx")
 
 
-    
 '''仿真LOOP'''
-MAX_EPISODE = 20        # 总的训练/评估次数
+from copy import deepcopy
+
+MAX_EPISODE = 5         # 总的训练/评估次数
 render = True           # 是否可视化训练/评估过程(仿真速度会降几百倍)
 
 for episode in range(MAX_EPISODE):
@@ -86,11 +39,10 @@ for episode in range(MAX_EPISODE):
         if render:
             env.render()
         # 决策
-        obs_tensor = {
-                'point': th.FloatTensor(obs['point']).unsqueeze(0).to("cpu"),
-                'state': th.FloatTensor(obs['state']).unsqueeze(0).to("cpu"),
-            }
-        act = policy.act(obs_tensor)
+        seq_points = obs['seq_points'].reshape(1, *obs['seq_points'].shape)                   # (1, seq_len, *points_shape, )
+        seq_vector = obs['seq_vector'].reshape(1, *obs['seq_vector'].shape)                   # (1, seq_len, vector_dim, )
+        act = policy.run(['action'], {'seq_points': seq_points, 'seq_vector': seq_vector})[0] # return [action, ...]
+        act = act.flatten()                                                                   # (1, dim, ) -> (dim, )
         # 仿真
         next_obs, _, _, info = env.step(act)
         # 回合结束
